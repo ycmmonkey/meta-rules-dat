fc519f7becb571cfe00f65e274ad4602ea8b0c9e
JJI <des@die.com>
2024-03-19 05:38:13 +0000

    update

--- resouces/bm7.sh
+++ resouces/bm7.sh
@@ -40,16 +40,16 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 	mkdir -p ${list[i]}
 	# 归类
 	# android package
-	if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep PROCESS | grep -v '\.exe' | grep -v '/' | grep '\.')" ]; then
-		cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' |  grep PROCESS | grep -v '\.exe' | grep -v '/' | grep '\.' | sed 's/  - PROCESS-NAME,//g' > ${list[i]}/package.json
-	fi
+	# if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep PROCESS | grep -v '\.exe' | grep -v '/' | grep '\.')" ]; then
+	# 	cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' |  grep PROCESS | grep -v '\.exe' | grep -v '/' | grep '\.' | sed 's/  - PROCESS-NAME,//g' > ${list[i]}/package.json
+	# fi
 	# process name
-	if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep PROCESS | grep -v '/' | grep -v '\.')" ]; then
-		cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep -v '#' | grep PROCESS | grep -v '/' | grep -v '\.' | sed 's/  - PROCESS-NAME,//g' > ${list[i]}/process.json
-	fi
-	if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep PROCESS |  grep '\.exe')" ]; then
-		cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep -v '#' | grep PROCESS |  grep '\.exe' | sed 's/  - PROCESS-NAME,//g' >> ${list[i]}/process.json
-	fi
+	# if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep PROCESS | grep -v '/' | grep -v '\.')" ]; then
+	# 	cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep -v '#' | grep PROCESS | grep -v '/' | grep -v '\.' | sed 's/  - PROCESS-NAME,//g' > ${list[i]}/process.json
+	# fi
+	# if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep PROCESS |  grep '\.exe')" ]; then
+	# 	cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep -v '#' | grep PROCESS |  grep '\.exe' | sed 's/  - PROCESS-NAME,//g' >> ${list[i]}/process.json
+	# fi
 	# domain
 	if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep '\- DOMAIN-SUFFIX,')" ]; then
 		cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep '\- DOMAIN-SUFFIX,' | sed 's/  - DOMAIN-SUFFIX,//g' > ${list[i]}/domain.json
@@ -62,24 +62,24 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 		cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep '\- DOMAIN-KEYWORD,' | sed 's/  - DOMAIN-KEYWORD,//g' > ${list[i]}/keyword.json
 	fi
 	# ipcidr
-	if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep '\- IP-CIDR')" ]; then
-		cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep '\- IP-CIDR' | sed 's/  - IP-CIDR,//g' | sed 's/  - IP-CIDR6,//g' > ${list[i]}/ipcidr.json
-	fi
+	# if [ -n "$(cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep '\- IP-CIDR')" ]; then
+	# 	cat ./rule/Clash/${list[i]}/${list[i]}.yaml | grep -v '#' | grep '\- IP-CIDR' | sed 's/  - IP-CIDR,//g' | sed 's/  - IP-CIDR6,//g' > ${list[i]}/ipcidr.json
+	# fi
 	# 转成json格式
 	# android package
-	if [ -f "${list[i]}/package.json" ]; then
-		sed -i 's/^/        "/g' ${list[i]}/package.json
-		sed -i 's/$/",/g' ${list[i]}/package.json
-		sed -i '1s/^/      "package_name": [\n/g' ${list[i]}/package.json
-		sed -i '$ s/,$/\n      ],/g' ${list[i]}/package.json
-	fi
+	# if [ -f "${list[i]}/package.json" ]; then
+	# 	sed -i 's/^/        "/g' ${list[i]}/package.json
+	# 	sed -i 's/$/",/g' ${list[i]}/package.json
+	# 	sed -i '1s/^/      "package_name": [\n/g' ${list[i]}/package.json
+	# 	sed -i '$ s/,$/\n      ],/g' ${list[i]}/package.json
+	# fi
 	# process name
-	if [ -f "${list[i]}/process.json" ]; then
-		sed -i 's/^/        "/g' ${list[i]}/process.json
-		sed -i 's/$/",/g' ${list[i]}/process.json
-		sed -i '1s/^/      "process_name": [\n/g' ${list[i]}/process.json
-		sed -i '$ s/,$/\n      ],/g' ${list[i]}/process.json
-	fi
+	# if [ -f "${list[i]}/process.json" ]; then
+	# 	sed -i 's/^/        "/g' ${list[i]}/process.json
+	# 	sed -i 's/$/",/g' ${list[i]}/process.json
+	# 	sed -i '1s/^/      "process_name": [\n/g' ${list[i]}/process.json
+	# 	sed -i '$ s/,$/\n      ],/g' ${list[i]}/process.json
+	# fi
 	# domain
 	if [ -f "${list[i]}/domain.json" ]; then
 		sed -i 's/^/        "/g' ${list[i]}/domain.json
@@ -100,23 +100,23 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 		sed -i '$ s/,$/\n      ],/g' ${list[i]}/keyword.json
 	fi
 	# ipcidr
-	if [ -f "${list[i]}/ipcidr.json" ]; then
-		sed -i 's/^/        "/g' ${list[i]}/ipcidr.json
-		sed -i 's/$/",/g' ${list[i]}/ipcidr.json
-		sed -i '1s/^/      "ip_cidr": [\n/g' ${list[i]}/ipcidr.json
-		sed -i '$ s/,$/\n      ],/g' ${list[i]}/ipcidr.json
+	# if [ -f "${list[i]}/ipcidr.json" ]; then
+	# 	sed -i 's/^/        "/g' ${list[i]}/ipcidr.json
+	# 	sed -i 's/$/",/g' ${list[i]}/ipcidr.json
+	# 	sed -i '1s/^/      "ip_cidr": [\n/g' ${list[i]}/ipcidr.json
+	# 	sed -i '$ s/,$/\n      ],/g' ${list[i]}/ipcidr.json
 	fi
 	# 合并文件
-	if [ -f "${list[i]}/package.json" -a -f "${list[i]}/process.json" ]; then
-		mv ${list[i]}/package.json ${list[i]}.json
-		sed -i '$ s/,$/\n    },\n    {/g' ${list[i]}.json
-		cat ${list[i]}/process.json >> ${list[i]}.json
-		rm ${list[i]}/process.json
-	elif [ -f "${list[i]}/package.json" ]; then
-		mv ${list[i]}/package.json ${list[i]}.json
-	elif [ -f "${list[i]}/process.json" ]; then
-		mv ${list[i]}/process.json ${list[i]}.json
-	fi
+	# if [ -f "${list[i]}/package.json" -a -f "${list[i]}/process.json" ]; then
+	# 	mv ${list[i]}/package.json ${list[i]}.json
+	# 	sed -i '$ s/,$/\n    },\n    {/g' ${list[i]}.json
+	# 	cat ${list[i]}/process.json >> ${list[i]}.json
+	# 	rm ${list[i]}/process.json
+	# elif [ -f "${list[i]}/package.json" ]; then
+	# 	mv ${list[i]}/package.json ${list[i]}.json
+	# elif [ -f "${list[i]}/process.json" ]; then
+	# 	mv ${list[i]}/process.json ${list[i]}.json
+	# fi
 
 	if [ "$(ls ${list[i]})" = "" ]; then
 		sed -i '1s/^/{\n  "version": 1,\n  "rules": [\n    {\n/g' ${list[i]}.json
